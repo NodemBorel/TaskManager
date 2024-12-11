e8f480f63b4504c01884b9256c64273548edb4e2
Tchouala Nodem Borel <80188664+NodemBorel@users.noreply.github.com>
2024-12-11 11:04:49 +0100

    maven add

--- src/main/java/com/taskmanager/DatabaseConnection.java
+++ src/main/java/com/taskmanager/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/task";
-    private static final String USER = "user";
-    private static final String PASSWORD = "password";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "user24";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
